9c26888a9b3cf9b9ef89846228d35202e2d4a611
Frederic Brodbeck <der.froedi@gmail.com>
2023-09-30 22:34:40 +0200

    write results to file

--- script.py
+++ script.py
@@ -9,6 +9,8 @@ from tqdm import tqdm
 
 C = bpy.context
 S = C.scene
+
+outfileName = 'results.txt'
 cyclesPrefs = C.preferences.addons['cycles'].preferences
 
 computeDeviceTypes = [
@@ -20,39 +22,6 @@ computeDeviceTypes = [
 	# 'ONEAPI',
 	'NONE',
 ]
-
-devTypesSupported = []
-for devType in computeDeviceTypes:
-	try:
-		cyclesPrefs.compute_device_type = devType
-		devTypesSupported.append(devType)
-	except TypeError:
-		print(f'{devType} not supported')
-
-paramGrid = ParameterGrid([
-	{
-		'devType': devTypesSupported,
-		'featureSet': [
-			'SUPPORTED',
-			'EXPERIMENTAL'
-		],
-		'tileSize': [
-			# 32,
-			64,
-			128,
-			256,
-			512,
-			1024,
-			2048,
-			4096
-		],
-		# 'persistentData': [True, False],
-		'useOSL': [
-			True,
-			False
-		],
-	}
-])
 
 
 def initMetadataBurning():
@@ -101,25 +70,62 @@ def render(params):
 	return duration
 
 
-# initMetadataBurning()
-S.render.engine = 'CYCLES'
-S.cycles.use_denoising = False
-S.cycles.use_auto_tile = True
-S.render.use_persistent_data = False
+def main(f):
+	# initMetadataBurning()
+	S.render.engine = 'CYCLES'
+	S.cycles.use_denoising = False
+	S.cycles.use_auto_tile = True
+	S.render.use_persistent_data = False
 
-results = []
-for params in tqdm(paramGrid):
-	duration = render(params)
-	results.append({
-		**params,
-		'renderTime': duration
-	})
+	devTypesSupported = []
+	for devType in computeDeviceTypes:
+		try:
+			cyclesPrefs.compute_device_type = devType
+			devTypesSupported.append(devType)
+		except TypeError:
+			msg = f'{devType} not supported'
+			print(msg)
+			f.write(f'{msg}\n')
 
-print()
-print('###############################')
-print('results:')
+	paramGrid = ParameterGrid([
+		{
+			'devType': devTypesSupported,
+			'featureSet': [
+				'SUPPORTED',
+				'EXPERIMENTAL'
+			],
+			'tileSize': [
+				# 32,
+				64,
+				128,
+				256,
+				512,
+				1024,
+				2048,
+				4096
+			],
+			# 'persistentData': [True, False],
+			'useOSL': [
+				True,
+				False
+			],
+		}
+	])
 
-for params in sorted(results, key=lambda x: x['renderTime']):
-	duration = params['renderTime']
-	del params['renderTime']
-	print(f"{duration:.3f}s _ {params}")
+	results = []
+	for params in tqdm(paramGrid):
+		duration = render(params)
+		results.append({ **params, 'renderTime': duration })
+
+	print()
+	print('###############################')
+	print('results:')
+	for params in sorted(results, key=lambda x: x['renderTime']):
+		duration = params['renderTime']
+		del params['renderTime']
+		print(f'{duration:.3f}s _ {params}')
+		f.write(f'{duration:.3f}s _ {json.dumps(params)}\n')
+
+
+with open(outfileName, 'w') as f:
+	main(f)
